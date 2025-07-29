a64fa2917acc8b291665ae2570427b98a90e66f6
lachose-jB <jadjahouisso@gmail.com>
2025-07-30 00:36:12 +0200

    update

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -61,6 +61,9 @@ const Header = () => {
               <a href="#blog" className="text-gray-700 hover:text-blue-600 transition-colors font-medium">
                 Blog
               </a>
+              <a href="#blog" className="text-gray-700 hover:text-blue-600 transition-colors font-medium">
+                Fondation
+              </a>
               <a href="#contact" className="text-gray-700 hover:text-blue-600 transition-colors font-medium">
                 Contact
               </a>
